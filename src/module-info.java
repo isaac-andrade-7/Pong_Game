3b82421a794374866bbd79222dd3b3b4ea0ca441
isaac <isaac.fariaspg@gmail.com>
2025-06-29 17:14:17 -0300

    janela

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Pong_Game {
+	requires java.desktop;
 }
--- src/pong_game/Main.java
+++ src/pong_game/Main.java
@@ -1,11 +1,55 @@
 package pong_game;
 
-public class Main {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
-    public static void main(String[] args) {
-        PongGame pongGame = new PongGame();
-        pongGame.start();
-        //qualquer coisa aqui
-    }
+import javax.swing.JFrame;
+
+public class Main extends Canvas implements Runnable{
+
+	//public static int WIDTH = 480, HEIGTH = 480;
+	public static int WIDTH = 480;
+	public static int HEIGTH = 480;
+	
+	public Main() {
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		new Thread(main).start();
+		
+		JFrame janela = new JFrame("Pong_Game");
+        janela.add(main);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setResizable(false);
+        janela.pack();
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+	    // Exemplo simples: desenhar uma bola
+	    g.setColor(Color.BLACK);
+	    g.fillRect(0, 0, WIDTH, HEIGHT); // fundo preto
+
+	    g.setColor(Color.WHITE);
+	    g.fillOval(220, 220, 20, 20); // uma bolinha no centro
+	}
+	
+	@Override
+	public void run() {
+	    while (true) {
+	        repaint(); // redesenha a tela
+	        try {
+	            Thread.sleep(16); // ~60 FPS
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
 
 }
