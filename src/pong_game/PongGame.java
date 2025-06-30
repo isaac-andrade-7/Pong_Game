6bc25f1722bc49e58201f7c6580665a79842fb6d
Tio Ivo <albertoivo@gmail.com>
2025-06-30 11:02:56 -0300

    Implement score tracking and reset functionality in PongGame
     Co-authored-by: isaac <isaac.fariaspg@gmail.com>

--- src/pong_game/PongGame.java
+++ src/pong_game/PongGame.java
@@ -14,4 +14,28 @@ public class PongGame {
         System.out.println("Pong Game Started!");
     }
 
+	public void updateScore(int player) {
+		if (player == 1) {
+			pontuacaoJogador1++;
+		} else if (player == 2) {
+			pontuacaoJogador2++;
+		}
+		System.out.println("Player 1 Score: " + pontuacaoJogador1);
+		System.out.println("Player 2 Score: " + pontuacaoJogador2);
+	}
+
+	public int getPontuacaoJogador1() {
+		return pontuacaoJogador1;
+	}
+
+	public int getPontuacaoJogador2() {
+		return pontuacaoJogador2;
+	}
+
+	public void resetScores() {
+		pontuacaoJogador1 = 0;
+		pontuacaoJogador2 = 0;
+		System.out.println("Scores have been reset.");
+	} 
+
 }
